4179842650df801ae371d839d4e35b797590461b
Lally Oppenheimer <wfihtuwflthwatw@dummygmail.com>
2013-06-08 20:37:26 -0500

    Added automatic package installation

--- init.el
+++ init.el
@@ -2,59 +2,57 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(require 'package) (package-initialize)
+(package-initialize)
 
-; I want spaces for indentation
-(setq-default indent-tabs-mode nil)
+;; loads the listed packages, installing if necessary
+(mapc
+ (lambda (package)
+   (or (package-installed-p package) 
+       (package-install package))
+   (require package))
+ '(dired+ magit rainbow-mode yasnippet package ido-vertical-mode ido-ubiquitous evil))
 
-;line numbers
-(global-linum-mode t)
+;;installs the following packages (without loading) if necessary
+(mapc
+ (lambda (package)
+   (or (package-installed-p package) 
+       (package-install package)))
+ '(auctex))
 
-(set-scroll-bar-mode 'right)   ; replace 'right with 'left to place it to the left
-;(set-face-background 'scroll-bar "red")
+(require 'recentf)
+    (recentf-mode 1)
+    (setq recentf-max-menu-items 50)
 
-
-;(require 'color-theme)
-;    (color-theme-initialize)
-;    (color-theme-late-night)
-
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)  
-(evil-mode 1)
-
-(add-to-list 'load-path "~/.emacs.d/evil/lalopmak-evil")
-(require 'lalopmak-evil)
-
-(require 'yasnippet) ;; not yasnippet-bundle
-
-    (yas--initialize)
-
+;; (iswitchb-mode t)
 (require 'ido)
-(ido-mode t)
+(ido-mode 'both) ;; for buffers and files
 
 (setq ido-enable-flex-matching t
-      ;;ido-everywhere t
+      ido-everywhere t
+      ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
+      ido-case-fold  t                 ; be case-insensitive
+      ido-enable-last-directory-history t ; remember last used dirs
+      ido-record-commands t
+      ido-max-work-directory-list 30   ; should be enough
+      ido-max-work-file-list      50   ; remember many
+      ido-use-virtual-buffers t 
+      )
 
- ido-enable-last-directory-history t ; remember last used dirs
-  ido-max-work-directory-list 30   ; should be enough
-  ido-max-work-file-list      50   ; remember many
-)
+;; This tab override shouldn't be necessary given ido's default 
+;; configuration, but minibuffer-complete otherwise dominates the 
+;; tab binding because of my custom tab-completion-everywhere 
+;; configuration.
+(add-hook 'ido-setup-hook 
+          (lambda () 
+            (define-key ido-completion-map [tab] 'ido-complete)))
 
-(require 'ido-vertical-mode)
+
+
+(yas--initialize)
+
 (ido-vertical-mode t)
 
-;; Use ido everywhere
-(require 'ido-ubiquitous)
 (ido-ubiquitous-mode 1)
-
 ;; Fix ido-ubiquitous for newer packages
 (defmacro ido-ubiquitous-use-new-completing-read (cmd package)
   `(eval-after-load ,package
@@ -67,13 +65,34 @@
 (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
 
 
-;; This tab override shouldn't be necessary given ido's default 
-;; configuration, but minibuffer-complete otherwise dominates the 
-;; tab binding because of my custom tab-completion-everywhere 
-;; configuration.
-(add-hook 'ido-setup-hook 
-          (lambda () 
-            (define-key ido-completion-map [tab] 'ido-complete)))
+;; I want spaces for indentation
+(setq-default indent-tabs-mode nil)
+
+;;line numbers
+(global-linum-mode t)
+
+(set-scroll-bar-mode 'right)   ; replace 'right with 'left to place it to the left
+;;(set-face-background 'scroll-bar "red")
+
+
+;;(require 'color-theme)
+;;    (color-theme-initialize)
+;;    (color-theme-late-night)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+
+(evil-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/lalopmak-evil")
+(require 'lalopmak-evil)
+
+    (show-paren-mode 1)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
@@ -83,7 +102,7 @@
 (require 'flymake)
 
 (defun flymake-get-tex-args (file-name)
-	(list "lualatex"
-	      (list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
+  (list "lualatex"
+        (list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
 
 (add-hook 'LaTeX-mode-hook 'flymake-mode)	
